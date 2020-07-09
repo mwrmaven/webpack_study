35c26d55c6f9398f28f16178cbef7c8d028dee70
mavenr <1227156208@qq.com>
2020-07-09 23:16:39 +0800

    结合webpack实现children子路由

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import Vue from 'vue'
 // 导入 Account、GoodsList 组件
 import account from './main/Account.vue'
 import goodslist from './main/GoodsList.vue'
+import login from './subcom/login.vue'
+import register from './subcom/register.vue'
 
 //1、 导入vue路由 vue-router
 import VueRouter from 'vue-router'
@@ -14,7 +16,14 @@ var router = new VueRouter({
     // 路由
     routes: [
         // 定义两个路由 account goodslist
-        {path: '/account', component: account},
+        {
+            path: '/account',
+            component: account,
+            children: [ // 子路由
+                { path: 'login', component: login}, // TODO 注意，子路由不能加斜杠 /
+                { path: 'register', component: register}
+            ]
+        },
         {path: "/goodslist", component: goodslist}
     ]
 });
--- src/main/Account.vue
+++ src/main/Account.vue
@@ -1,6 +1,10 @@
 <template>
     <div>
         <h1>这是 account 组件</h1>
+
+        <router-link to="/account/login">登陆</router-link>
+        <router-link to="/account/register">注册</router-link>
+        <router-view></router-view>
     </div>
 </template>
 
